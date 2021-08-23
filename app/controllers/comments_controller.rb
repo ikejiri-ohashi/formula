78f6757b7ab1d85e8706c47abba2092536c47a7b
ikejiri-ohashi <ikejiri.nabe@gmail.com>
2021-08-24 07:10:15 +0900

    コメント投稿とそれを詳細画面に表示する実装
    ※ここでquestionsコントローラーの不要な記述を削除

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,17 @@
 class CommentsController < ApplicationController
+
+  def create
+    @comment = Comment.create(comment_params)
+    if @comment.save
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, question_id: params[:question_id])
+  end
+
 end
