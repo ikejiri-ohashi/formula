750b14369103df4aa90a923bbe68d1b692bfd079
ikejiri-ohashi <ikejiri.nabe@gmail.com>
2021-08-24 07:59:31 +0900

    非同期通信のためのリクエストを送信する実装

--- app/javascript/comment.js
+++ app/javascript/comment.js
@@ -1,8 +1,13 @@
 function question (){
   const submit = document.getElementById("submit");
-  submit.addEventListener("click", () => {
-    const form = document.getElementById("form-control");
+  submit.addEventListener("click", (e) => {
+    e.preventDefault();
+    const form = document.getElementById("form");
     const formData = new FormData(form);
+    const XHR = new XMLHttpRequest();
+    XHR.open("POST", "/questions/:question_id/comments " , true);
+    XHR.responseType = "json";
+    XHR.send(formData);
   });
 };
 
