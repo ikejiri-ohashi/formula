dae48d97ffc4f284ea8837cdb8fcec5b9d4ba48f
ikejiri-ohashi <ikejiri.nabe@gmail.com>
2021-08-24 17:58:01 +0900

    共感したボタンの非同期通信の実装

---
+++ app/javascript/like.js
@@ -0,0 +1,24 @@
+function question (){
+  const like = document.getElementById("like");
+  like.addEventListener("click", (e) => {
+    e.preventDefault();
+    const like = document.getElementById("form");
+    const likeData = new FormData(like);
+    const XHR = new XMLHttpRequest();
+    var question_id = document.getElementById("hidden_id").value;
+    XHR.open("POST", "/questions/" + question_id + "/likes " , true);
+    XHR.responseType = "json";
+    XHR.send(likeData);
+    XHR.onload = () => {
+      if (XHR.status != 200) {
+        alert(`Error ${XHR.status}: ${XHR.statusText}`);
+        return null;
+      };
+      const list = document.getElementById("add_like");
+      const html = `あなたと`;
+      list.insertAdjacentHTML("afterend", html);
+    };
+  });
+};
+
+window.addEventListener('load', question);
