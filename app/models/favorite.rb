456a191762baa7c94a6554a9601ea23f411ad263
ikejiri-ohashi <ikejiri.nabe@gmail.com>
2021-09-04 09:27:54 +0900

    fix3

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,5 +1,5 @@
 class Favorite < ApplicationRecord
   belongs_to :answer
   belongs_to :user
-  validates :answer_id, uniqueness: { scope: :user }
+  validates :answer_id, uniqueness: { scope: :user_id }
 end
