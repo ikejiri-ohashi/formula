c192aa493f233e03fa7109c04d22eb44fe02797f
ikejiri-ohashi <ikejiri.nabe@gmail.com>
2021-09-06 17:49:53 +0900

    fix7

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -62,7 +62,7 @@
 server '35.72.79.96', user: 'ec2-user', roles: %w{app db web}
 
 set :ssh_options, {
-  keys: [ENV.fetch('PRODUCTION_SSH_KEY').to_s],
+  keys: %w(~/.ssh/ikejirinabekey.pem),
   forward_agent: true,
   auth_methods: %w[publickey]
 }
